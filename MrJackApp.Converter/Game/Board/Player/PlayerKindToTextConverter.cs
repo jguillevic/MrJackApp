062a1d0c5426afa2ac3e5c9443ce40978eb034b3
jguillevic <guillevicjerome@yahoo.fr>
2018-03-16 17:30:16 +0100

    Avancées sur l'affichage du joueur.

---
+++ MrJackApp.Converter/Game/Board/Player/PlayerKindToTextConverter.cs
@@ -0,0 +1,40 @@
+﻿using MrJackApp.ViewModel.Game.Board.Player;
+using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MrJackApp.Converter.Game.Board.Player
+{
+    public sealed class PlayerKindToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kind = (PlayerKind)value;
+
+            var sb = new StringBuilder();
+            sb.Append("Vous êtes ");
+
+            switch (kind)
+            {
+                case PlayerKind.Undefined:
+                    throw new ArgumentException();
+                case PlayerKind.Jack:
+                    sb.Append("Jack");
+                    break;
+                case PlayerKind.Inspector:
+                    sb.Append("L'inspecteur");
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            return sb.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
